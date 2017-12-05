6dc5d10fed298450f48182b1f2f59f6b3a055c12
Deven Phillips <deven.phillips@redhat.com>
2017-12-05 12:26:56 -0500

    Configure web security headers

---
+++ src/main/java/com/redhat/labs/demo/spring/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.redhat.labs.demo.spring;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends
+        WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .headers()
+            .xssProtection()
+            .block(true);
+    }
+}
